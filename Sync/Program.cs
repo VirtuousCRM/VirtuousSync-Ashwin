962d400678a1a819eae898e3925a33c07c400f5e
ashwin-2610 <ashwin.2610@gmail.com>
2024-12-20 16:41:03 -0700

    Ashwin-Assessment: Bug Fixes

--- Sync/Program.cs
+++ Sync/Program.cs
@@ -15,26 +15,37 @@ namespace Sync
 
         private static async Task Sync()
         {
-            var apiKey = "REPLACE_WITH_API_KEY_PROVIDED";
+            var apiKey = "v_eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiN2VhYTBhNTQtYTBiZC00OTNlLWFjNDMtZjNjZGEwZmVlNWQ5IiwiZXhwIjoyMTQ3NDgzNjQ3LCJpc3MiOiJodHRwczovL2FwcC52aXJ0dW91c3NvZnR3YXJlLmNvbSIsImF1ZCI6Imh0dHBzOi8vYXBpLnZpcnR1b3Vzc29mdHdhcmUuY29tIn0.oN0bfmYMS7lPxGtVH3ouEVhD0Kuzoqa2nAnuvPTyPpk";
             var configuration = new Configuration(apiKey);
             var virtuousService = new VirtuousService(configuration);
 
             var skip = 0;
             var take = 100;
             var maxContacts = 1000;
-            var hasMore = true;
 
-            using (var writer = new StreamWriter($"Contacts_{DateTime.Now:MM_dd_yyyy}.csv"))
-            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            if (take > 0 && maxContacts > 0)
             {
-                do
+                //if value of maxContacts is less than value of take, then we only retrieve maxContacts number of contacts
+                take = Math.Min(take, maxContacts);
+
+                using (var writer = new StreamWriter($"Contacts_{DateTime.Now:MM_dd_yyyy}.csv"))
+                using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
                 {
-                    var contacts = await virtuousService.GetContactsAsync(skip, take);
-                    skip += take;
-                    csv.WriteRecords(contacts.List);
-                    hasMore = skip > maxContacts;
+                    bool hasMore;
+
+                    do
+                    {
+                        var contacts = await virtuousService.GetContactsAsync(skip, take);
+                        skip += take;
+                        csv.WriteRecords(contacts.List);
+                        hasMore = skip < maxContacts;
+                    }
+                    while (hasMore);
                 }
-                while (!hasMore);
+            }
+            else
+            {
+                Console.WriteLine("maxContacts should be greater than 0");
             }
         }
     }
